7835fce85a01a352796af146c8384b8fd8f9e78c
sjnaj <2471326731@qq.com>
2021-05-19 20:00:52 +0800

    cpp1

---
+++ practice.cpp
@@ -0,0 +1,15 @@
+/*
+ * @Author: fengsc
+ * @Date: 2021-05-19 17:22:12
+ * @LastEditTime: 2021-05-19 19:12:07
+ */
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a,b;
+    cin>>a>>b;
+    cout<<a<<b<<'\n';
+    return 0;
+
+}
